65505e8ae014d2dab3097239039d7f0483bc2ca6
Nico <nicoalmram@gmail.com>
2022-05-19 14:12:35 +0100

    cli-pages y alt-img

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyC4qlPVT7vo9So3SiR6vb107JqURBUI3Nc",
+    authDomain: "crud-angular-235b5.firebaseapp.com",
+    projectId: "crud-angular-235b5",
+    storageBucket: "crud-angular-235b5.appspot.com",
+    messagingSenderId: "749515066147",
+    appId: "1:749515066147:web:d347a20d1df1819c5f1a96"
+  }
 };
